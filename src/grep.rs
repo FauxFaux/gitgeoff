d78b81c34d85cd01f3084b82c5f33e755df26106
Chris West (Faux) <git@goeswhere.com>
2019-11-05 21:09:56 +0000

    chore: extract method

--- src/grep.rs
+++ src/grep.rs
@@ -16,24 +16,29 @@ pub(crate) fn grep(pattern: &str) -> Result<(), Error> {
                 return Ok(());
             }
             let repo = git2::Repository::open(dest)?;
-            let tree_obj = repo.revparse_single("origin/REMOTE_HEAD")?.peel_to_tree()?;
-            tree_obj.walk(git2::TreeWalkMode::PostOrder, |dir, entry| {
-                match entry.kind() {
-                    Some(git2::ObjectType::Blob) => (),
-                    _ => return git2::TreeWalkResult::Ok,
-                };
-                let object = entry.to_object(&repo).expect("TODO: can't load blob");
-
-                let content = object.as_blob().expect("type checked above").content();
-
-                if twoway::find_bytes(content, pattern.as_bytes()).is_some() {
-                    println!("{:?} {:?} {:?}", dest, dir, entry.name());
-                }
-
-                git2::TreeWalkResult::Ok
-            })?;
-            Ok((()))
+            grep_in(pattern, &format!("{:?}", dest), &repo)?;
+            Ok(())
         })
         .collect::<Result<_, _>>()?;
+    Ok(())
+}
+
+fn grep_in(pattern: &str, prefix: &str, repo: &git2::Repository) -> Result<(), Error> {
+    let tree_obj = repo.revparse_single("origin/REMOTE_HEAD")?.peel_to_tree()?;
+    tree_obj.walk(git2::TreeWalkMode::PostOrder, |dir, entry| {
+        match entry.kind() {
+            Some(git2::ObjectType::Blob) => (),
+            _ => return git2::TreeWalkResult::Ok,
+        };
+        let object = entry.to_object(&repo).expect("TODO: can't load blob");
+
+        let content = object.as_blob().expect("type checked above").content();
+
+        if twoway::find_bytes(content, pattern.as_bytes()).is_some() {
+            println!("{:?} {:?} {:?}", prefix, dir, entry.name());
+        }
+
+        git2::TreeWalkResult::Ok
+    })?;
     Ok(())
 }
