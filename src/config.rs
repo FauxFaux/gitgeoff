5a584f79629555a1aee6b18e001e5a778cb526e1
Chris West (Faux) <git@goeswhere.com>
2020-12-09 17:40:22 +0000

    chore: better error for missing config

--- src/config.rs
+++ src/config.rs
@@ -3,11 +3,13 @@ use std::fs;
 use std::io;
 use std::io::BufRead;
 use std::io::Read;
+use std::path::Path;
 use std::str::FromStr;
 
 use anyhow::anyhow;
 use anyhow::Context;
 use anyhow::Error;
+use anyhow::Result;
 use url::Url;
 
 use crate::git_url::GitUrl;
@@ -26,7 +28,7 @@ impl Spec {
 
 pub fn load() -> Result<Vec<Spec>, Error> {
     let mut ret = Vec::with_capacity(20);
-    let file = io::BufReader::new(fs::File::open(".gitgeoff")?);
+    let file = io::BufReader::new(open(".gitgeoff")?);
     for line in file.lines() {
         let line = line?;
         if line.is_empty() {
@@ -45,4 +47,12 @@ pub fn load() -> Result<Vec<Spec>, Error> {
     }
 
     Ok(ret)
+}
+
+fn open<P: AsRef<Path>>(path: P) -> Result<fs::File> {
+    let path = path.as_ref();
+    fs::File::open(path).with_context(|| match fs::canonicalize(path) {
+        Ok(path) => anyhow!("opening {:?}", path),
+        Err(_) => anyhow!("resolving {:?}", path),
+    })
 }
